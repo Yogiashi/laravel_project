212a5c8bb467b7da567329d615302951754cefc6
border <yogiashi.takuya@gmail.com>
2023-10-23 16:53:58 +0900

    modify category_create

--- laravel_project/MoneyCanvas/app/Http/Controllers/CategoryController.php
+++ laravel_project/MoneyCanvas/app/Http/Controllers/CategoryController.php
@@ -8,6 +8,7 @@ use App\Category;
 use App\User;
 use App\Http\Requests\BlogPostRequest;
 use Illuminate\Support\Facades\Auth;
+use App\Jobs\TestJob;
 
 class CategoryController extends Controller
 {
@@ -28,14 +29,7 @@ class CategoryController extends Controller
         $user = auth()->id();
         $inputs = $request->all();
 
-        \DB::beginTransaction();
-        try {
-            Category::create($inputs);
-            \DB::commit();
-        }catch(\Throwable $e) {
-            \DB::rollback();
-            abort(500);
-        }
+        TestJob::dispatch($user, $inputs);
      
         return redirect(route('categoryNew'))->with('flash_message', 'カテゴリを登録しました。');
     }
